4a859dee3100939ec6c8f5d8d69c9b90881f3433
Aadil Waghat <a.waghat16@gmail.com>
2025-09-23 12:37:13 +0100

    iitial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.time.LocalDateTime;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Clock! Time now is: " + LocalDateTime.now() );
+    }
+}
